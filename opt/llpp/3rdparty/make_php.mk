8d410f3c2be12804a355b1eeb1ceb6d81f0fc2bd
Florian Krull <flo.krull@gmail.com>
2020-05-21 16:36:23 +0200

    llpp: 3rdparty php use postgresql dep

--- opt/llpp/3rdparty/make_php.mk
+++ opt/llpp/3rdparty/make_php.mk
@@ -22,12 +22,12 @@ download/php-${LIBVER_php}.tar.gz:
 	mkdir -p download/
 	cd download && wget https://www.php.net/distributions/php-${LIBVER_php}.tar.gz
 
-php/bin/php-cgi: download/php-${LIBVER_php}.tar.gz
+php/bin/php-cgi: download/php-${LIBVER_php}.tar.gz | postgresql_build
 	mkdir -p php_build \
 		&& cd php_build \
 		&& tar xf ../download/php-${LIBVER_php}.tar.gz \
 		&& cd php-$(LIBVER_php) \
-		&& ./configure --prefix=$(BASEDIR)/php --without-libxml --without-sqlite3 --disable-dom --without-pdo-sqlite --disable-simplexml --disable-xml --disable-xmlreader --disable-xmlwriter
+		&& ./configure --prefix=$(BASEDIR)/php --without-libxml --without-sqlite3 --disable-dom --without-pdo-sqlite --disable-simplexml --disable-xml --disable-xmlreader --disable-xmlwriter --with-pgsql=$(BASEDIR)/postgresql --with-pdo-pgsql=$(BASEDIR)/postgresql
 	$(MAKE) -C php_build/php-$(LIBVER_php)
 	$(MAKE) -C php_build/php-$(LIBVER_php) install
 	$(RM) -r php_build
